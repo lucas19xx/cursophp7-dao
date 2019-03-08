8ea47740c0daf3a9dc7848b2378fbf421772d72a
Lucas <lucasferreirac99@hotmail.com>
2019-03-08 19:31:50 -0300

    Métodos de listagem

--- index.php
+++ index.php
@@ -2,12 +2,23 @@
 
 require_once ("config.php");
 
-$lucas = new Usuario();
-$lucas->loadbyId(3);
+//Carrega apenas um usuario
+//$lucas = new Usuario();
+//$lucas->loadbyId(3);
+//echo $lucas;
 
-echo $lucas;
+//Carrega uma lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
+//Carrega uma lista dos usuarios buscando pelo login
+//$search = Usuario::search("lu");
+//echo json_encode($search);
+
+//Carrega um usuario usando login e senha(autenticação)
+$usuario = new Usuario();
+$usuario->login("lucas", "123456");
+
+echo $usuario;
 
 ?>
-
-
